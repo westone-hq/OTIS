b1faa0fb345d8eeac8859b837cb87d2a3394a1eb
nada <namespace.nada@gmail.com>
2026-07-04 10:04:08 +0900

    [P0] 참조 소스 추출 파일 인코딩 교정

---
+++ reference/develop/lib/models/sensor_sample.dart
@@ -0,0 +1,13 @@
+﻿class SensorSample {
+  const SensorSample({
+    required this.timestamp,
+    required this.x,
+    required this.y,
+    required this.z,
+  });
+
+  final DateTime timestamp;
+  final double x;
+  final double y;
+  final double z;
+}
